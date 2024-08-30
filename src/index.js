55fabba2c69ee3da380abd2a88f5c5fce2eb85fa
Benjamin Barbe <benjamin@pandasuite.com>
2024-08-30 11:34:24 +0200

    First step for pdf editor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,338 @@
 import PandaBridge from "pandasuite-bridge";
 import "./index.css";
+import { PDFDocument } from "pdf-lib";
+
+let pdfDocViewer = null;
+let infiniteViewer = null;
+let moveable = null;
+let selecto = null;
+let targets = [];
+
+let totalPages = 0;
+let currentPage = 1;
+let lastValidPage = 1;
+
+const positions = [];
+let scaleFactor = 1;
+let panOffset = { x: 0, y: 0 };
+let originalPDFWidth = 0;
+let originalPDFHeight = 0;
+const minZoom = 0.5;
+const maxZoom = 3;
 
 let properties = null;
 let markers = null;
 
-function myInit() {
-  // const imageUrl = PandaBridge.resolvePath('my_image.png');
-  // PandaBridge.send('imageChanged');
+async function getPdfUrl() {
+  const url = PandaBridge.resolvePath("document.pdf");
+
+  if (!url) {
+    return null;
+  }
+
+  const files = await fetch(`${url}.extract-content.json`)
+    .then((res) => res.json())
+    .catch(() => {
+      console.log("No extract-content.json file", url);
+      return null;
+    });
+
+  if (files) {
+    const file = files.find((f) => f.endsWith(".pdf"));
+
+    if (file) {
+      return `${url}${file}?no_redirect`;
+    }
+  }
+  return null;
+}
+
+async function getOrCreatePdf() {
+  const url = await getPdfUrl();
+
+  if (url) {
+    const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
+    return PDFDocument.load(existingPdfBytes);
+  }
+  return PDFDocument.create();
+}
+
+async function updatePdf(pdfDoc) {
+  const pdfBytes = await pdfDoc.save();
+  const blob = new Blob([pdfBytes], { type: "application/pdf" });
+  const url = URL.createObjectURL(blob);
+
+  return url;
+}
+
+function initToolbar() {
+  const pageCount = document.getElementById("pageCount");
+  pageCount.innerText = totalPages;
+
+  const toolbar = document.getElementById("toolbar-content");
+  toolbar.style.opacity = 1;
+
+  const currentPageInput = document.getElementById("currentPageInput");
+  currentPageInput.max = totalPages;
+
+  currentPageInput.addEventListener("change", (e) => {
+    const pageNumber = parseInt(e.target.value, 10);
+    if (pageNumber >= 1 && pageNumber <= totalPages) {
+      currentPage = pageNumber;
+      lastValidPage = pageNumber;
+      infiniteViewer.setZoom(1);
+      renderPage(currentPage);
+    } else {
+      e.target.value = lastValidPage;
+    }
+  });
+}
+
+async function initPdfViewer(url) {
+  const pdfjs = await import("pdfjs-dist/legacy/build/pdf");
+  const pdfjsWorker = await import("pdfjs-dist/build/pdf.worker.entry");
+  pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+  const { getDocument } = pdfjs;
+
+  pdfDocViewer = await getDocument({ url }).promise;
+  totalPages = pdfDocViewer.numPages;
+  initToolbar();
+
+  await initInfiniteViewer();
+  await initMoveable();
+  await initSelecto();
+  await renderPage(currentPage);
+}
+
+async function renderPage(pageNumber) {
+  const page = await pdfDocViewer.getPage(pageNumber);
+
+  const viewport = page.getViewport({ scale: 1 });
+  const container = document.getElementById("pdf-container");
+  const canvas = document.getElementById("pdf-canvas");
+  const context = canvas.getContext("2d");
+
+  const containerWidth = container.clientWidth;
+  const containerHeight = container.clientHeight;
+
+  const minScaleFactor = Math.min(
+    containerWidth / viewport.width,
+    containerHeight / viewport.height,
+  );
+
+  originalPDFWidth = viewport.width;
+  originalPDFHeight = viewport.height;
+
+  const resolutionScale = (window.devicePixelRatio || 1) * maxZoom;
+
+  const scaledViewport = page.getViewport({
+    scale: minScaleFactor * resolutionScale,
+  });
+
+  canvas.width = scaledViewport.width;
+  canvas.height = scaledViewport.height;
+
+  canvas.style.width = `${scaledViewport.width / resolutionScale}px`;
+  canvas.style.height = `${scaledViewport.height / resolutionScale}px`;
+
+  canvas.style.opacity = 0;
+  await page.render({ canvasContext: context, viewport: scaledViewport });
+  infiniteViewer.scrollCenter();
+  canvas.style.opacity = 1;
+
+  updateZonesOnTransform();
+}
+
+function addInteractiveZone(rect) {
+  const newDiv = document.createElement("div");
+  newDiv.className = "zone-interactive";
+  document.getElementById("pdf-container").appendChild(newDiv);
+
+  const containerRect = document
+    .getElementById("pdf-canvas")
+    .getBoundingClientRect();
+  const position = calculatePosition(rect, containerRect);
+  positions.push(position);
+
+  updateZoneStyle(newDiv, position, containerRect);
+}
+
+function updateZoneStyle(zone, position, containerRect) {
+  const scaleX = containerRect.width / originalPDFWidth;
+  const scaleY = containerRect.height / originalPDFHeight;
+
+  const absoluteX = position.x * scaleX - panOffset.x * scaleFactor;
+  const absoluteY = position.y * scaleY - panOffset.y * scaleFactor;
+  const absoluteWidth = position.width * scaleX;
+  const absoluteHeight = position.height * scaleY;
+
+  zone.style.transform = `translate(${absoluteX}px, ${absoluteY}px)`;
+  zone.style.width = `${absoluteWidth}px`;
+  zone.style.height = `${absoluteHeight}px`;
+}
+
+function updateZonesOnTransform() {
+  if (moveable.target.length > 0) {
+    setTargets([]);
+  }
+
+  const containerRect = document
+    .getElementById("pdf-canvas")
+    .getBoundingClientRect();
+
+  document.querySelectorAll(".zone-interactive").forEach((zone, index) => {
+    updateZoneStyle(zone, positions[index], containerRect);
+  });
+
+  // moveable.updateTarget();
+}
+
+async function initInfiniteViewer() {
+  const { default: InfiniteViewer } = await import("infinite-viewer");
+
+  infiniteViewer = new InfiniteViewer(
+    document.getElementById("pdf-container"),
+    {
+      zoom: 1,
+      usePinch: true,
+      useWheelScroll: true,
+      useAutoZoom: true,
+      zoomRange: [minZoom, maxZoom],
+      useTransform: true,
+    },
+  );
+
+  infiniteViewer.on("scroll", (e) => {
+    panOffset = { x: e.scrollLeft, y: e.scrollTop };
+    updateZonesOnTransform();
+  });
+
+  infiniteViewer.on("pinch", (e) => {
+    scaleFactor = e.zoom;
+    updateZonesOnTransform();
+  });
+}
+
+function setTargets(newTargets) {
+  targets = newTargets;
+  moveable.target = targets;
+}
+
+async function initMoveable() {
+  const { default: Moveable } = await import("moveable");
+
+  moveable = new Moveable(document.body, {
+    target: targets,
+    draggable: true,
+    resizable: true,
+    keepRatio: false,
+  });
+
+  moveable.on("drag", (e) => {
+    e.target.style.transform = e.transform;
+  });
+
+  moveable.on("dragEnd", () => {
+    updatePositions();
+  });
+
+  moveable.on("resize", (e) => {
+    e.target.style.width = `${e.width}px`;
+    e.target.style.height = `${e.height}px`;
+    e.target.style.transform = e.drag.transform;
+  });
+
+  moveable.on("resizeEnd", () => {
+    updatePositions();
+  });
+}
+
+async function initSelecto() {
+  const { default: Selecto } = await import("selecto");
+
+  selecto = new Selecto({
+    container: document.body,
+    selectableTargets: [".zone-interactive"],
+    selectByClick: true,
+    selectFromInside: false,
+    hitRate: 0,
+    ratio: 0,
+  });
+
+  // selecto.on("select", (e) => {
+  //   e.added.forEach((el) => el.classList.add("selected"));
+  //   e.removed.forEach((el) => el.classList.remove("selected"));
+  // });
+
+  selecto.on("dragStart", (e) => {
+    const { inputEvent } = e;
+    const { target } = inputEvent;
+
+    if (
+      moveable.isMoveableElement(target) ||
+      targets.some((t) => t === target || t.contains(target))
+    ) {
+      e.stop();
+    }
+  });
+
+  selecto.on("dragEnd", (e) => {
+    if (
+      e.inputEvent.target.className.includes("zone-interactive") ||
+      !e.isDrag
+    ) {
+      return;
+    }
+    addInteractiveZone(e.rect);
+  });
+
+  selecto.on("selectEnd", (e) => {
+    if (e.isDragStart) {
+      e.inputEvent.preventDefault();
+      moveable.waitToChangeTarget().then(() => {
+        moveable.dragStart(e.inputEvent);
+      });
+    }
+    setTargets(e.selected);
+  });
+}
+
+function calculatePosition(rect, containerRect) {
+  const x =
+    (rect.left - containerRect.left) * (originalPDFWidth / containerRect.width);
+  const y =
+    (rect.top - containerRect.top) * (originalPDFHeight / containerRect.height);
+  const width = (rect.width / containerRect.width) * originalPDFWidth;
+  const height = (rect.height / containerRect.height) * originalPDFHeight;
+
+  return { x, y, width, height };
+}
+
+function updatePositions() {
+  const containerRect = document
+    .getElementById("pdf-canvas")
+    .getBoundingClientRect();
+
+  positions.length = 0;
+
+  document.querySelectorAll(".zone-interactive").forEach((zone) => {
+    const rect = zone.getBoundingClientRect();
+    positions.push(calculatePosition(rect, containerRect));
+  });
+}
+
+async function onDomLoaded() {
+  const pdfDoc = await getOrCreatePdf();
+
+  const form = pdfDoc.getForm();
+  const fields = form?.getFields();
+  // loop through the fields and print their names and values
+  fields.forEach((field) => {
+    // field.enableReadOnly();
+  });
+
+  const url = await updatePdf(pdfDoc);
+  initPdfViewer(url);
 }
 
 PandaBridge.init(() => {
@@ -15,9 +341,9 @@ PandaBridge.init(() => {
     markers = pandaData.markers;
 
     if (document.readyState === "complete") {
-      myInit();
+      onDomLoaded();
     } else {
-      document.addEventListener("DOMContentLoaded", myInit, false);
+      document.addEventListener("DOMContentLoaded", onDomLoaded, false);
     }
   });
 
